fbcffad2a7021a1c32554b1eeff0c00cee8d8a0a
Faizan <faizanamjad575@gmail.com>
2025-06-27 18:20:35 +0500

    scripts for external links

---
+++ script.js
@@ -0,0 +1,55 @@
+document.getElementById("battleship-github").addEventListener("click", () => {
+  window.open("https://github.com/Faizan0994/Battleship", "_blank");
+});
+
+document.getElementById("battleship-live").addEventListener("click", () => {
+  window.open("https://faizan0994.github.io/Battleship", "_blank");
+});
+
+document.getElementById("weather-app-github").addEventListener("click", () => {
+  window.open("https://github.com/Faizan0994/weatherApp", "_blank");
+});
+
+document.getElementById("weather-app-live").addEventListener("click", () => {
+  window.open("https://faizan0994.github.io/weatherApp/", "_blank");
+});
+
+document
+  .getElementById("admin-dashboard-github")
+  .addEventListener("click", () => {
+    window.open("https://github.com/Faizan0994/TOP-admin-dashboard", "_blank");
+  });
+
+document
+  .getElementById("admin-dashboard-live")
+  .addEventListener("click", () => {
+    window.open("https://faizan0994.github.io/TOP-admin-dashboard", "_blank");
+  });
+
+document.getElementById("sign-up-form-github").addEventListener("click", () => {
+  window.open("https://github.com/Faizan0994/TOP-sign-up-form", "_blank");
+});
+
+document.getElementById("sign-up-form-live").addEventListener("click", () => {
+  window.open("https://faizan0994.github.io/TOP-sign-up-form", "_blank");
+});
+
+document
+  .getElementById("restaurant-page-github")
+  .addEventListener("click", () => {
+    window.open("https://github.com/Faizan0994/restaurant-page", "_blank");
+  });
+
+document
+  .getElementById("restaurant-page-live")
+  .addEventListener("click", () => {
+    window.open("https://faizan0994.github.io/restaurant-page", "_blank");
+  });
+
+document.getElementById("calculator-github").addEventListener("click", () => {
+  window.open("https://github.com/Faizan0994/TOP_calculator", "_blank");
+});
+
+document.getElementById("calculator-live").addEventListener("click", () => {
+  window.open("https://faizan0994.github.io/TOP_calculator", "_blank");
+});
